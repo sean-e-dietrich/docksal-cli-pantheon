7ea56f3dd36e173f26096ac75b67464c6025ce25
Sean Dietrich <sean.dietrich@inresonance.com>
2017-06-05 15:54:33 -0700

    Added groups to the way programs interact with running data

--- auth_terminus.sh
+++ auth_terminus.sh
@@ -2,7 +2,17 @@
 
 TERMINUS_TOKEN=${TERMINUS_TOKEN:=} 
 
+if [ ! -d /home/docker/.terminus ]; then
+  sudo mkdir /home/docker/.terminus
+fi
+chown -R docker:100 /home/docker/.terminus
+
+if [ ! -d /home/docker.drush ]; then
+  sudo mkdir /home/docker/.drush
+fi
+chown -R docker:100 /home/docker/.drush
+
 if [ ! -z $TERMINUS_TOKEN ]; then
-  sudo -u docker /opt/terminus/bin/terminus auth:login --machine-token=$TERMINUS_TOKEN
-  sudo -u docker /opt/terminus/bin/terminus auth:whoami
+  sudo -u docker -g 100 /opt/terminus/bin/terminus auth:login --machine-token=$TERMINUS_TOKEN
+  sudo -u docker -g 100 /opt/terminus/bin/terminus auth:whoami
 fi
