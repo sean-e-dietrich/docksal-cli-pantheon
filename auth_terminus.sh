71abdc74fdb594990d7887448680c9e627c6f63d
Sean Dietrich <sean.dietrich@inresonance.com>
2017-06-05 11:39:55 -0700

    Setting owner of terminus folder

--- auth_terminus.sh
+++ auth_terminus.sh
@@ -2,6 +2,8 @@
 
 TERMINUS_TOKEN=${TERMINUS_TOKEN:=} 
 
+sudo chown -R docker:100 /home/docker/.terminus
+
 if [ ! -z $TERMINUS_TOKEN ]; then
   sudo -u docker /opt/terminus/bin/terminus auth:login --machine-token=$TERMINUS_TOKEN
   sudo -u docker /opt/terminus/bin/terminus auth:whoami
