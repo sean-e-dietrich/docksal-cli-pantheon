1a10cb982b2399c3057785ee5f3dccbd2fcfc420
Sean Dietrich <sean.dietrich@inresonance.com>
2017-06-07 10:38:37 -0700

    Setting so terminus does not restart and setting permissions for docker entrypoint

--- auth_terminus.sh
+++ auth_terminus.sh
@@ -5,14 +5,14 @@ TERMINUS_TOKEN=${TERMINUS_TOKEN:=}
 if [ ! -d /home/docker/.terminus ]; then
   sudo mkdir /home/docker/.terminus
 fi
-chown -R docker:100 /home/docker/.terminus
+chown -R "$HOST_UID:$HOST_GID" /home/docker/.terminus
 
 if [ ! -d /home/docker.drush ]; then
   sudo mkdir /home/docker/.drush
 fi
-chown -R docker:100 /home/docker/.drush
+chown -R "$HOST_UID:$HOST_GID" /home/docker/.drush
 
 if [ ! -z $TERMINUS_TOKEN ]; then
-  sudo -u docker -g 100 /opt/terminus/bin/terminus auth:login --machine-token=$TERMINUS_TOKEN
-  sudo -u docker -g 100 /opt/terminus/bin/terminus auth:whoami
+  sudo -u $HOST_UID -g $HOST_GID /opt/terminus/bin/terminus auth:login --machine-token=$TERMINUS_TOKEN
+  sudo -u $HOST_UID -g $HOST_GID /opt/terminus/bin/terminus auth:whoami
 fi
