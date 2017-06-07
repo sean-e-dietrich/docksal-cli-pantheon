3c3a85d9fa14283f1ae1f1887207029f021e2a11
Sean Dietrich <sean.dietrich@inresonance.com>
2017-06-07 13:43:04 -0700

    running chmod in sudo mode

--- auth_terminus.sh
+++ auth_terminus.sh
@@ -5,12 +5,12 @@ TERMINUS_TOKEN=${TERMINUS_TOKEN:=}
 if [ ! -d /home/docker/.terminus ]; then
   sudo mkdir /home/docker/.terminus
 fi
-chown -R "$HOST_UID:$HOST_GID" /home/docker/.terminus
+sudo chown -R "$HOST_UID:$HOST_GID" /home/docker/.terminus
 
 if [ ! -d /home/docker.drush ]; then
   sudo mkdir /home/docker/.drush
 fi
-chown -R "$HOST_UID:$HOST_GID" /home/docker/.drush
+sudo chown -R "$HOST_UID:$HOST_GID" /home/docker/.drush
 
 if [ ! -z $TERMINUS_TOKEN ]; then
   sudo -u $HOST_UID -g $HOST_GID /opt/terminus/bin/terminus auth:login --machine-token=$TERMINUS_TOKEN
